ee25454731ac3b3384485b0d1abd3a996df2a69e
bahaa1985 <bahaa_salaheldin@live.com>
2024-05-06 13:06:46 +0300

    game problem 2

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -8,26 +8,29 @@ import  getGame from '../Api/getGame.js'
 function Game(){
 
     const [gameData,setGameData]=useState([]);
-    const [fixtureId,setFixtureId]=useState(parseInt(useParams().fixtureId));
-    // setFixtureId(parseInt(useParams()))
-    useEffect(()=>{             
-        getGame(fixtureId).then((result)=>{
+    // const [fixtureId,setFixtureId]=useState(useParams().fixtureId);
+    const params=useParams();
+    const [fixtureId,setFixtureId]=useState(0);
+    console.log("fixture id:",fixtureId);
+    useEffect(()=>{       
+        setFixtureId(params.fixtureId);
+         getGame(fixtureId).then((result)=>{
             setGameData(result.data.response[0])
         })
-        console.log("gd",gameData);
+        
     },
     [fixtureId,gameData]
 )
-    
+console.log("gd",gameData);
     return(
-        <div>                    
+        <div>                  
             <div key={fixtureId} className="fixture-teams">
-              <img alt="" src={gameData.teams.home.logo}></img>
-              <span className="team">{gameData.teams.home.name}</span>
-              <span className="result">{gameData.goals.home}</span>
-              <span className="result">{gameData.goals.away}</span>
-              <span className="team">{gameData.teams.away.name}</span>
-              <img alt="" src={gameData.teams.away.logo}></img>
+              <img alt="" src={gameData?.teams.home.logo}></img>
+              <span className="team">{gameData?.teams.home?.name}</span>
+              <span className="result">{gameData?.goals.home}</span>
+              <span className="result">{gameData?.goals.away}</span>
+              <span className="team">{gameData?.teams.away.name}</span>
+              <img alt="" src={gameData?.teams.away.logo}></img>
             </div>
         </div>
     )
