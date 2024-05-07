5d84001249bdafe6f8a133a4e4b950e76645b068
bahaa1985 <bahaa_salaheldin@live.com>
2024-05-07 20:39:37 +0300

    game problem is fixed

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,5 +1,5 @@
 import {React, useState, useEffect} from 'react'
-import { useParams  } from 'react-router-dom'
+import { useParams, useLocation  } from 'react-router-dom'
 import  getGame from '../Api/getGame.js' 
 // import Events from "./Events.js";
 // import Statistics from "./Statistics.js";
@@ -9,30 +9,28 @@ function Game(){
 
     const [gameData,setGameData]=useState([]);
     const params=useParams();
-    // const [fixtureId,setFixtureId]=useState(0);
-    console.log("fixture id:",params.fixtureId);
+    const [fixtureId,setFixtureId]=useState(0);   
    
-    useEffect(()=>{       
-        // setFixtureId(params.fixtureId);
+    useEffect(()=>{               
          getGame(params.fixtureId).then((result)=>{
             setGameData(result.data.response[0])
         })
-        console.log(`game${params.fixtureId}`);
+        console.log(`game${fixtureId}`);
     },
-    [params.fixtureId,gameData]
+    [params.fixtureId,fixtureId]
 )
 console.log("gd",gameData);
 
     return(
-        <div>                  
-            {/* <div key={fixtureId} className="fixture-teams">
-              <img alt="" src={gameData?.teams.home.logo}></img>
-              <span className="team">{gameData?.teams.home?.name}</span>
-              <span className="result">{gameData?.goals.home}</span>
-              <span className="result">{gameData?.goals.away}</span>
-              <span className="team">{gameData?.teams.away.name}</span>
-              <img alt="" src={gameData?.teams.away.logo}></img>
-            </div> */}
+        <div>                            
+            <div key={gameData?.fixture?.id} className="fixture-teams">
+              <img alt="" src={gameData?.teams?.home?.logo}></img>
+              <span className="team">{gameData?.teams?.home?.name}</span>
+              <span className="result">{gameData?.goals?.home}</span>
+              <span className="result">{gameData?.goals?.away}</span>
+              <span className="team">{gameData?.teams?.away?.name}</span>
+              <img alt="" src={gameData?.teams?.away?.logo}></img>
+            </div>
         </div>
     )
 }
