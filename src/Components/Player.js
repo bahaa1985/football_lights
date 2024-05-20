687cb269873a68b3096807c646fa17b5539fb6e8
bahaa1985 <bahaa_salaheldin@live.com>
2024-05-20 22:54:22 +0300

    create season and league dropboxes

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -1,100 +1,114 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { getPlayerStats } from '../Api/getPlayerProfile.js';
+import { getPlayerSeasons, getPlayerStats } from '../Api/getPlayerProfile.js';
 
 function Player(props) {
     const season=props.season;
+    const [playerSeasons,setPlayerSeasons]=useState([]);
     const [playerStats,setPlayerStats]=useState([]); 
-    const [seasonIndex,setSeasonIndex]=useState(0);
-    const [selectedSeason,setSelectedSeason]=useState(0);
+    const [leagueId,setLeagueId]=useState(0);
+    const [selectedSeason,setSelectedSeason]=useState(season);
     const params =useParams();
     
     useEffect(()=>{
-        getPlayerStats(params.playerId,season)
+
+        getPlayerSeasons(params.playerId)
+        .then((result)=>{
+            setPlayerSeasons(result.data.response);
+          
+        })
+
+        getPlayerStats(params.playerId,selectedSeason)
         .then((result)=>{
             setPlayerStats(result.data.response[0]);   
         });               
        
-    },[params.playerId,season])   
+    },[params.playerId,selectedSeason])
+    
+    
+    // function seasonsDropDownBox(){
+    //     let seasonArr=[];
+    //     for(let i=2012;i<2025;i++){
+    //         seasonArr.push(i);
+    //     }
+    //     return seasonArr
+    // }
 
-    function playerStatsArr(seasonIndex){
+    function playerStatsArr(leagueId){
         let arr=[]
-        Object.entries(playerStats.statistics[seasonIndex]).map((elem)=>{
+        Object.entries(playerStats.statistics.filter(e=>e.league.id===leagueId)).map((elem)=>{
             elem.map((elem)=>{
                 arr.push(elem);
             })
         })
-        return  arr
+        console.log("arr",arr);
+        return  arr[1]
     }
-    let seasonArr=[];
+   
+    console.log("player stats",playerStats);
+    console.log("league id",leagueId);
     return ( 
         <div>
             <div>
                 <h2>{playerStats?.player?.name}</h2>
                 <img src={playerStats?.player?.photo} alt={playerStats?.player?.name}/>
                 <div>
-                <select onChange={(e)=>setSelectedSeason(parseInt(e.target.value))}>
-                {[
-                        playerStats?.statistics?.map((item,index)=>{
-                        seasonArr.push(item.league.season)
-                    })
-                    ,
-                    playerStats?.statistics?.map((item,index)=>{                  
-                        return(                                
-                            <option key={index}  value={index}><div>{item.league.season}</div></option>
-                        )
-                    })]
-                }
-                </select>
-                <select onChange={(e)=>setSeasonIndex(parseInt(e.target.value))}>
-                {
-                    playerStats?.statistics?.filter(stat=>stat.league.season===selectedSeason).map((item,index)=>{                  
-                        return(                                
-                            <option key={index}  value={index}><div>{item.league.name}<img src={item.league.flag} alt={item.league.name}/></div></option>
-                        )
-                    })
-                }
-                </select>
-                </div>
-                
-               
+                    <select onChange={(e)=>setSelectedSeason(parseInt(e.target.value))}>
+                    {                        
+                       playerSeasons?.map((item,index)=>{
+                            return(
+                                <option key={index} value={item}>{item}</option>
+                            )
+                        })            
+                    }
+                    </select>
+
+                    <select onChange={(e)=>setLeagueId(parseInt(e.target.value))}> 
+                    {
+                        playerStats?.statistics?.map((item,index)=>{                  
+                            return(                                
+                                <option key={index}  value={item.league.id}><div>{item.league.name}</div></option>
+                            )
+                        })
+                    }
+                    </select>
+
+                </div>                               
             </div>
             <div>
                 {
                     playerStats?.statistics? 
-                    Object.entries(playerStatsArr(seasonIndex)).map((element,index) => {
+                    playerStatsArr(leagueId)
+                    // .filter(item=>item?.league?.id===leagueId)
+                    .map((element,index) => {
                         return(
                             index > 3 ?
                             <div key={index}>
                                 {
                                     index % 2 === 0 ?
                                     <p>{element[1][0].toUpperCase()+element[1].slice(1)}</p>
-                                    : 
-                                    <div>
-                                        {
-                                            element.map((elem,index)=>{
+                                    :     
+                                    element.map((elem,index)=>{
+                                        return(
+                                            Object.entries(elem).map((ele,index)=>{
                                                 return(
-                                                    Object.entries(elem).map((ele,index)=>{
-                                                        return(
-                                                            <div key={index}> 
+                                                    <div key={index}> 
+                                                    {
+                                                        isNaN(ele[0]) ?
+                                                        <>
+                                                            <span>{ele[0][0].toUpperCase()+ele[0].slice(1)}</span>
                                                             {
-                                                                isNaN(ele[0]) ?
-                                                                <>
-                                                                    <span>{ele[0][0].toUpperCase()+ele[0].slice(1)}</span>
-                                                                    {
-                                                                        ele[1] === null ?   <span>NA</span> :   <span>{ele[1]}</span>
-                                                                    }
-                                                               </>
-                                                                :null
-                                                            }                                                                                                              
-                                                            </div>                                                    
-                                                        )
-                                                      
-                                                    })
+                                                                ele[1] === null ?   <span>NA</span> :   <span>{ele[1]}</span>
+                                                            }
+                                                        </>
+                                                        :null
+                                                    }                                                                                                              
+                                                    </div>                                                    
                                                 )
+                                                
                                             })
-                                        }
-                                    </div>
+                                        )
+                                    })
                                 }
                             </div>
                             :null
--- src/api/getPlayerProfile.js
+++ src/api/getPlayerProfile.js
@@ -1,5 +1,19 @@
 import axios from "axios";
 
+export function getPlayerSeasons(playerId){
+    const config={
+        method:"GET",
+        url:`https://v3.football.api-sports.io/players/seasons?player=${playerId}`,
+        headers: {
+            'x-rapidapi-host': 'v3.football.api-sports.io',
+            'x-rapidapi-key': '12c3d051c8f77e0840cd9c5e35fd8cd0'
+        },
+
+    };
+
+    return axios(config);
+}
+
 export function getPlayerStats(playerId,season){
     const config={
         method:"GET",
