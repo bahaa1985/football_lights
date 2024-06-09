cbe97278eabf479907066ce754d97f839ce76949
bahaa1985 <bahaa_salaheldin@live.com>
2024-06-09 04:45:47 +0300

    preference component

--- src/Components/Preference.js
+++ src/Components/Preference.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js'
 import { getLeagues } from "../Api/getLeaguesTeams.js";
 import { getTeam } from "../Api/getLeaguesTeams.js";
 import Cookies from "universal-cookie";
@@ -18,16 +20,6 @@ export default function Preferences(params) {
   useEffect(() => {
     if(searchLeague.trim().length>0){
       getLeagues(searchLeague).then((result) => {
-        // setLeagues(result.data.response);
-        // for(let i=0;i<result.data.response.length;i++){
-        //   // leagues[i].selected=true;
-        //   if(selectedLeagues.indexOf(result.data.response[i].league.id)===-1) {
-        //     result.data.response[i].selected=false;
-        //   }
-        //   else{
-        //     result.data.response[i].selected=true;
-        //   }
-        // };
         setLeagues(result.data.response);
         
       });
@@ -51,14 +43,12 @@ export default function Preferences(params) {
     cookie.set("preferedTeams", selectedTeams);
   }
 
-  function addSelectedLeagueToArray(elem){
-    if(selectedLeagues.indexOf(elem.league.id) === -1){
-      selectedLeagues.push(parseInt(elem.league.id))
-      return true;
+  function handleSelectedLeague(elem,leagueId=0){
+    if(elem !== null && selectedLeagues.indexOf(selectedLeagues.filter(elem=>elem.league.id===leagueId)[0]) === -1){
+      setSelectedLeagues(selectedLeagues.push(elem));
     }
     else {
-      selectedLeagues.splice(selectedLeagues.indexOf(parseInt(elem.league.id)),1)
-      return false;
+      setSelectedLeagues(selectedLeagues.splice(selectedLeagues.indexOf(selectedLeagues.filter(elem=>elem.league.id===leagueId)[0]),1));
     }
   }
 
@@ -71,59 +61,54 @@ export default function Preferences(params) {
     }
   }
   
-  function toggleBackgroundColor(){
-    if(backgroundSelected === 'white');
-  }
-  console.log("leagues",leagues);
-console.log("selected",selectedLeagues);
+// console.log("leagues",leagues);
+// console.log("selected",selectedLeagues);
 
   return (
     <div>
-      {/* search leagues */}
-      <div>
-        <input type="text" ref={searchLeagueInput} />
-        <button
-          onClick={() => setSearchLeague(searchLeagueInput.current.value)}
-        >
-          SearchLeague
-        </button>
-        {leagues?.map((elem, index) => {
-          return (
-            <div key={index} onClick={()=>{
-              addSelectedLeagueToArray(elem);
-              console.log("selected",selectedLeagues);
-              backgroundSelected === 'white' ? setBackgroundSelected('lightgreen'): setBackgroundSelected('white')
-            }}          
-            style={{backgroundColor: (isLeagueSelected(elem.league.id) ? 'lightgreen':'white')}}>
-              <img
-                src={elem.league?.logo}
-                style={{ width: "40px", height: "40px" }}
-                alt={elem.league.name}
-              /> 
-              {elem.league.name} {elem.league.id} ({elem.country.name})         
-                           
-              {/* <input
-                type="checkbox" value={elem.league.id} checked={elem.selected}
-                onChange={(e)=>[
-                  e.target.checked
-                    ? addSelectedLeagueToArray(elem) : removeFromSelectedLeagueArray(elem)
-                  ,console.log(selectedLeagues)]
-                }
-              /> */}
-            </div>
-          );
-        })}
-        {/* <div>
-          {selectedLeagues.map((elem, index) => {
-            return <div>{elem}</div>;
+      <div className="container-fluid d-flex flex-wrap text-center">
+        <div className="col-sm-6 text-start">
+          <input type="text" ref={searchLeagueInput} />
+          <button
+            onClick={() => setSearchLeague(searchLeagueInput.current.value)}
+          >
+            SearchLeague
+          </button>
+          {leagues?.map((elem, index) => {
+            return (
+              <div key={index} onClick={()=>{
+                handleSelectedLeague(elem);
+                console.log("selected",selectedLeagues);
+                backgroundSelected === 'white' ? setBackgroundSelected('lightgreen'): setBackgroundSelected('white')
+              }}          
+              style={{backgroundColor: (isLeagueSelected(elem.league.id) ? 'lightgreen':'white')}}>
+                <img
+                  src={elem.league?.logo}
+                  style={{ width: "40px", height: "40px" }}
+                  alt={elem.league.name}
+                /> 
+                {elem.league.name} {elem.league.id} ({elem.country.name})         
+              </div>
+            );
           })}
-        </div> */}
-        <div>
+        </div>
+        {/*  */}
+        <div className="col-sm-6">
+          {selectedLeagues?.map((elem, index) => {
+            return (
+                  <div key={index} onClick={()=>[handleSelectedLeague(elem,elem.league.id),console.log("selected",selectedLeagues)]}>
+                    <img src={elem.league.logo} alt={elem.league.name}/>
+                    <div>{elem.league.name}</div>
+                  </div>
+                )
+          })}
           <button onClick={() => handleLeaguesCookie()}>
             Selected Leagues
           </button>
         </div>
       </div>
+      {/* search leagues */}
+     
       {/* search teams */}
       <div>
         <input type="text" ref={searchTeamInput} />
