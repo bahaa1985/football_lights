a026f13f265d6b28fb8b86a0a1dc6823c38580bb
bahaa1985 <bahaa_salaheldin@live.com>
2024-08-28 12:57:48 +0300

    preference stars

--- src/Components/Preference.js
+++ src/Components/Preference.js
@@ -15,7 +15,7 @@ export default function Preferences(params) {
   const [leaguesIds,setLeaguesIds]=useState([]);
   const [preferedTeams,setPreferedTeams]=useState([]);
   const [teamsIds,setTeamsIds]=useState([]);
-  const [isActive, setIsActive] = useState(false);
+  const [activeIndex, setIsActiveIndex] = useState(0);
   const [backgroundSelected,setBackgroundSelected]=useState('white');
   const searchLeagueInput = useRef("");
   const searchTeamInput = useRef("");
@@ -36,38 +36,38 @@ export default function Preferences(params) {
     }
 
   }, [searchLeague, searchTeam]);
-
- 
+  
+  let preferedLeaguesArr=[];  
   function handlePreferedLeagues(leagueId,season){
-    let preferedArr=[];
+    
     console.log("prefered leagues: ",getCookies("prefered_leagues"));
-    preferedArr=getCookies("prefered_leagues");
-    if(preferedArr.filter(obj=>obj.id===leagueId)[0] === undefined){
-      preferedArr.push({'id':leagueId,'season':season});
-      console.log("selected leagues: ",preferedArr);
+    preferedLeaguesArr=getCookies("prefered_leagues");
+    if(preferedLeaguesArr.filter(obj=>obj.id===leagueId)[0] === undefined){
+      preferedLeaguesArr.push({'id':leagueId,'season':season});
+      console.log("selected leagues: ",preferedLeaguesArr);
     }
     else{
-      const index=preferedArr.indexOf(preferedArr.filter(obj=>obj.id===leagueId)[0])
-      preferedArr=preferedArr.slice(0,index).concat(preferedArr.slice(index+1));
-      console.log("selected leagues: ",preferedArr);    
+      const index=preferedLeaguesArr.indexOf(preferedLeaguesArr.filter(obj=>obj.id===leagueId)[0])
+      preferedLeaguesArr=preferedLeaguesArr.slice(0,index).concat(preferedLeaguesArr.slice(index+1));
+      console.log("selected leagues: ",preferedLeaguesArr);    
     }
-    setPreferedLeagues(preferedArr);
+    setCookies(preferedLeaguesArr,"prefered_leagues");
   }
-
+  
+  let preferedTeamsArr=[];
   function handlePreferedTeams(teamId){
-    let preferedArr=[];
     console.log("prefered leagues: ",getCookies("prefered_leagues"));
-    preferedArr=getCookies("prefered_teams");
-    if(preferedArr.filter(obj=>obj.id===teamId)[0] === undefined){
-      preferedArr.push({'id':teamId});
-      console.log(preferedArr);
+    preferedTeamsArr=getCookies("prefered_teams");
+    if(preferedTeamsArr.filter(obj=>obj.id===teamId)[0] === undefined){
+      preferedTeamsArr.push({'id':teamId});
+      console.log(preferedTeamsArr);
     }
     else{
-      const index=preferedArr.indexOf(preferedArr.filter(obj=>obj.id===teamId)[0])
-      preferedArr=preferedArr.slice(0,index).concat(preferedArr.slice(index+1));
-      console.log(preferedArr);      
+      const index=preferedLeaguesArr.indexOf(preferedLeaguesArr.filter(obj=>obj.id===teamId)[0])
+      preferedLeaguesArr=preferedLeaguesArr.slice(0,index).concat(preferedLeaguesArr.slice(index+1));
+      console.log(preferedLeaguesArr);      
     }
-    setPreferedTeams(preferedArr);
+    
   }
 
 
@@ -79,9 +79,9 @@ export default function Preferences(params) {
 
   // console.log("prefered leagues",preferedLeagues);
   return (
-    <div className="relative top-20 left-0 p-4">
-      <div className="flex flex-wrap text-center">
-        <div className="text-left">
+    <div className="relative top-20 left-0 px-auto flex justify-around">
+      <div className="flex flex-wrap text-center w-[45%]">
+        <div className="text-left w-full">
           <input type="text" ref={searchLeagueInput} className="outline" />
           <button className="w-26 text-lg mx-2 p-2  rounded-md bg-slate-900 text-[#fff]"
             onClick={() => [setSearchLeague(searchLeagueInput.current.value),console.log(searchLeagueInput.current.value)]}>
@@ -89,38 +89,45 @@ export default function Preferences(params) {
           </button>
           {leagues?.map((elem, index) => {
             return (
-              <div key={index} className="cursor-pointer" onClick={()=>{
-                const latestSeasonIndex = elem.seasons.length-1;
-                handlePreferedLeagues(elem.league.id,elem.seasons[latestSeasonIndex].year);
-                backgroundSelected === 'white' ? setBackgroundSelected('lightgreen'): setBackgroundSelected('white')
-              }}          
+              <div key={index}
+                className="w-full flex justify-start my-2" 
+              //   onClick={()=>{
+              //   const latestSeasonIndex = elem.seasons.length-1;
+              //   handlePreferedLeagues(elem.league.id,elem.seasons[latestSeasonIndex].year);
+              //   backgroundSelected === 'white' ? setBackgroundSelected('lightgreen'): setBackgroundSelected('white')
+              // }}          
               // style={{backgroundColor: (isLeagueSelected(elem.league.id) ? 'lightgreen':'white')}}
               >
                 <img
                   src={elem.league?.logo}
-                  style={{ width: "40px", height: "40px" }}
+                  className="w-10 h-10"
                   alt={elem.league.name}
                 /> 
-                {elem.league.name} ({elem.country.name})         
-                <FontAwesomeIcon icon={faStar} 
-                                 className={`border-black ${preferedLeagues.find(ele=>ele.id===elem.league.id) ? "text-blue-700" : "text-white"}`} 
-                                 onClick={()=>setIsActive(!isActive)}/>
+                <span className="w-[70%] border-b border-b-black">{elem.league.name} {elem.league.id}</span>         
+                <FontAwesomeIcon icon={faStar}
+                                 className={`${'star'+index}  w-10 h-10 bg-black cursor-pointer ${preferedLeagues.find((item)=>item.id===elem.league.id) ? "text-blue-700" : "text-white"}`} 
+                                 onClick={()=>
+                                 {
+                                  // index===activeIndex ? setIsActiveIndex(-1) : setIsActiveIndex(index);
+                                  // console.log("active index",activeIndex);
+                                  const latestSeasonIndex = elem.seasons.length-1;
+                                  handlePreferedLeagues(elem.league.id,elem.seasons[latestSeasonIndex].year)}}/>
               </div>
             );
           })}
         </div>
         {/*  */}
-        <div>
+        {/* <div>
           <button className="w-auto text-lg mx-2 p-2  rounded-md bg-slate-900 text-[#fff]" 
           onClick={() => [setCookies(preferedLeagues,"prefered_leagues"),setLeagues([])]}>
             Save
           </button>
-        </div>
+        </div> */}
       </div>
       {/* search leagues */}
      
       {/* search teams */}
-      <div className="flex flex-wrap text-center mt-4">
+      <div className="flex flex-wrap text-center w-[45%]">
               <div className="text-left">
             <input type="text" ref={searchTeamInput} className="outline" />
             <button className="w-26 text-lg mx-2 p-2  rounded-md bg-slate-900 text-[#fff]" onClick={() => setSearchTeam(searchTeamInput.current.value)}>
