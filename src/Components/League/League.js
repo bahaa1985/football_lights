9ba9f7cae0b2d0b15e60a492020eda4a160889b8
bahaa1985 <bahaa_salaheldin@live.com>
2025-02-20 19:36:30 +0200

    redesign league v1

--- src/Components/League/League.js
+++ src/Components/League/League.js
@@ -27,28 +27,29 @@ export default function League() {
   const [selectedSeason,setSelectedSeason] = useState(season ? season : lastSeason);
   const [leagueInfo,setLeagueInfo]=useState();
   const [isLoaded,setLoaded]=useState(false);
-  const [isClicked,setIsClicked]=useState(false);
+  const [isDisabled, setDisabled] = useState(false);
+  const [isClicked,setClicked]=useState(false);
 
   useEffect(()=>{
-    if(isClicked === true){
+    if(isClicked){
       async function fetchData(){ 
         const leagueData = await getLeagues(null, selectedleague);
         setLeagueInfo(leagueData.data.response[0]);
-        setIsClicked(false);
+        setClicked(false);
         setLoaded(true);
       }
       fetchData();    
     }
-  },[])
+  },[isClicked,selectedleague])
 
   function handleSelectedLeague(leagueId){
     setSelectedLeague(leagueId);
-    setIsClicked(true);
+    setDisabled(false);
   }
 
   function handleSelectedSeason(e){
     setSelectedSeason(e.target.value);
-    setIsClicked(true);
+    setDisabled(false);
   }
   return (
     <div>
@@ -56,35 +57,38 @@ export default function League() {
       {/* Leagues dropdown */}
       <div className="relative top-20 left-[50%] -translate-x-[50%] w-[90%]">
 
-      <div>
+      <div className="my-4">
         <div>
           <span className="text-slate-900">Leagues</span>
-            <div className="">
+            <div className="w-full flex flex-wrap justify-start">
             {
-              leagues.length>0?
-              <TabGroup className="bg-slate-800" onChange={(index) => handleSelectedLeague(leagues[index].id)}>
-                <TabList className={`flex flex-row justify-start gap-2`}>
-                  {
-                    leagues.map((league,index)=>{
-                      return(
-                        <Tab key={index} className="rounded-lg bg-transparent text-slate-50 data-[selected]:bg-slate-600 hover:bg-slate-600">
-                          <img className="w-10 h-10 rounded" src={league.logo} alt={league.name} />
-                          <span className="text-[1em] border-none">{league.name}</span>
-                        </Tab>
-                      )
-                    })
-                  }
-                </TabList>
-              </TabGroup>
-              // <select onChange={(e)=>handleSelectedLeague(e)} value={selectedleague}>
-              //   {
-              //     leagues.map((league,index)=>{
-              //       return(
-              //         <option key={index} value={league.id}>{league.name}</option>
-              //       )
-              //     })
-              //   }
-              // </select>            
+              leagues.length > 0 ?
+              leagues.map((league,index)=>{
+                return(
+                  <button key={index} className={`w-20 border-2 border-solid border-slate-800 text-slate-50 rounded-3xl cursor-pointer
+                    ${selectedleague === league.id ? 'bg-slate-600 cursor-default' : 'bg-slate-800 cursor-pointer' }`} 
+                    disabled={selectedleague === league.id ? true : false} 
+                    onClick={()=>setSelectedLeague(league.id)}>
+                    <img className="w-10 h-10 rounded" src={league.logo} alt={league.name} />
+                    <span className="text-[1em] border-none">{league.name}</span>
+                  </button>
+                )
+              })
+              // <TabGroup className="bg-slate-800" defaultIndex={-1}
+              // onChange={(index) => [handleSelectedLeague(leagues[index].id),console.log(leagues[index].id)]}>
+              //   <TabList className={`flex flex-row justify-start gap-2`}>
+              //     {
+              //       leagues.map((league,index)=>{
+              //         return(
+              //           <Tab key={index} className="rounded-lg bg-transparent w-40 text-slate-50 data-[selected]:bg-slate-600 hover:bg-slate-600">
+              //             <img className="w-10 h-10 rounded" src={league.logo} alt={league.name} />
+              //             <span className="text-[1em] border-none">{league.name}</span>
+              //           </Tab>
+              //         )
+              //       })
+              //     }
+              //   </TabList>
+              // </TabGroup>           
               :
               null
             }
@@ -93,7 +97,7 @@ export default function League() {
       {/* seasons dropdown  */}
       <div className="">
           <span className="bg-slate-800 text-slate-50">Seasons</span>
-          <select onChange={(e)=>handleSelectedSeason(e)} defaultValue={lastSeason} value={selectedSeason}>
+          <select onChange={(e)=>handleSelectedSeason(e)} value={selectedSeason}>
             {
               seasons().map((season,index)=>{
                 return(
@@ -104,8 +108,9 @@ export default function League() {
           </select>
       </div>
       {/*  */}
-      <button className={`rounded-lg bg-slate-800 w-20 p-2 mx-auto ${isClicked ? 'cursor-pointer' : 'cursor-default'}`} onClick={()=>setIsClicked(true)} 
-      disabled={isClicked ? true : false}>
+      <button className={`rounded-lg bg-slate-800 text-slate-50 w-20 p-2 mx-auto ${!isDisabled ? 'cursor-pointer bg-slate-400 text-slate-900' : 'cursor-default bg-slate-800'}`} 
+      onClick={()=>[setClicked(true),setDisabled(true)]} 
+      disabled={isDisabled ? true : false}>
         OK
       </button>
       {/*  */}
@@ -127,23 +132,33 @@ export default function League() {
         </div>
         :null
       }
-      <TabGroup onChange={(index) => setTab(index)}>
+      <div className="w-full flex justify-between flex-wrap my-4 rounded-3xl border-2 border-solid border-slate-800">
+        <button className={`w-20 sm:w-[22%] h-12 bg-slate-800 text-slate-50 rounded-3xl ${tab === 0 ? 'bg-slate-600' : 'bg-slate-800'}`} onClick={()=>setTab(0)}>Fixtures</button>
+        <button className={`w-20 sm:w-[22%] h-12 bg-slate-800 text-slate-50 rounded-3xl ${tab === 1 ? 'bg-slate-600' : 'bg-slate-800'}`} onClick={()=>setTab(1)}>Standing</button>
+        <button className={`w-20 sm:w-[22%] h-12 bg-slate-800 text-slate-50 rounded-3xl ${tab === 2 ? 'bg-slate-600' : 'bg-slate-800'}`} onClick={()=>setTab(2)}>Scorers</button>
+        <button className={`w-20 sm:w-[22%] h-12 bg-slate-800 text-slate-50 rounded-3xl ${tab === 3 ? 'bg-slate-600' : 'bg-slate-800'}`} onClick={()=>setTab(3)}>Assisters</button>
+      </div>
+      {/* <TabGroup defaultIndex={null} onChange={(index) =>[setTab(index)]}>
         <TabList>
           <Tab className="bg-slate-500 text-slate-900 rounded-md mx-2 p-4 data-[selected]:bg-slate-900 data-[selected]:text-white">Fixtures</Tab>
           <Tab className="bg-slate-500 text-slate-900 rounded-md mx-2 p-4 data-[selected]:bg-slate-900 data-[selected]:text-white">Standing</Tab>
           <Tab className="bg-slate-500 text-slate-900 rounded-md mx-2 p-4 data-[selected]:bg-slate-900 data-[selected]:text-white">Scorers</Tab>
           <Tab className="bg-slate-500 text-slate-900 rounded-md mx-2 p-4 data-[selected]:bg-slate-900 data-[selected]:text-white">Assists</Tab>
         </TabList>
-      </TabGroup>
-      {tab === 0 ? (
-        <LeagueFixtures league={selectedleague} season={selectedSeason} />
-      ) : tab === 1 ? (
-        <Standings league={selectedleague} season={selectedSeason} />
-      ) : tab === 2 ? (
-        <TopPlayers league={selectedleague} season={selectedSeason} type={"Goals"} />
-      ) : tab === 3 ? (
-        <TopPlayers league={selectedleague} season={selectedSeason} type={"Assists"} />
-      ) : null}
+      </TabGroup> */}
+      { 
+        selectedleague && selectedSeason && isDisabled ?
+          tab === 0 ? 
+            <LeagueFixtures league={selectedleague} season={selectedSeason} />
+           : tab === 1 ? 
+            <Standings league={selectedleague} season={selectedSeason} />
+           : tab === 2 ? 
+            <TopPlayers league={selectedleague} season={selectedSeason} type={"Goals"} />
+           : tab === 3 ? 
+            <TopPlayers league={selectedleague} season={selectedSeason} type={"Assists"} />
+           : null
+        :null
+      }
       </div>
     </div>
   );
--- src/Navbar.js
+++ src/Navbar.js
@@ -29,15 +29,15 @@ function Navbar() {
 
     return ( 
         <div>
-            <nav id="nav_bar" class="bg-slate-900 w-full h-16 p-1 fixed top-0 left-0 flex no-wrap justify-between lg:justify-start shadow-md z-40">
+            <nav id="nav_bar" className="bg-slate-900 w-full h-16 p-1 fixed top-0 left-0 flex no-wrap justify-between lg:justify-start shadow-md z-40">
       {/* Hamburger button */}
       <div className="my-auto md:hidden basis-1/3">
         <button id="hamburger_button" ref={hamburger_button}>
-          <svg id="hamburger_lines" class="h-12 w-12 stroke-slate-100" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          <svg id="hamburger_lines" className="h-12 w-12 stroke-slate-100" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
           </svg>
-          <svg id="hamburger_close" class="hidden h-12 w-12 stroke-gray-100" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+          <svg id="hamburger_close" className="hidden h-12 w-12 stroke-gray-100" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
           </svg>
         </button>
       </div>
@@ -66,7 +66,7 @@ function Navbar() {
         </div>
         {/* Search bar */}
         <div className="px-3 md:flex justify-between hidden my-auto">
-          <svg id="search" viewBox="0 0 512 512" class="h-6 w-6 cursor-pointer p-0.5 rounded-l-md fill-gray-800 bg-[#fff]" title="search">
+          <svg id="search" viewBox="0 0 512 512" className="h-6 w-6 cursor-pointer p-0.5 rounded-l-md fill-gray-800 bg-[#fff]" title="search">
             <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z" />
           </svg>
           <input type="text" className="w-36 rounded-r-md bg-[#fff] outline-none" />
@@ -84,10 +84,10 @@ function Navbar() {
         {/* search bar */}
         <li className="liclass mx-5 py-2">
           <div className="flex justify-between my-auto">
-            <svg viewBox="0 0 512 512" class="w-6 cursor-pointer p-0.5 rounded-l-md bg-[#fff]" title="search" fill="#1f2937">
+            <svg viewBox="0 0 512 512" className="w-6 cursor-pointer p-0.5 rounded-l-md bg-[#fff]" title="search" fill="#1f2937">
               <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z" />
             </svg>
-            <input type="text" class=" rounded-r-md bg-[#fff]  outline-none " />
+            <input type="text" className=" rounded-r-md bg-[#fff]  outline-none " />
           </div>
         </li>
       </ul>
